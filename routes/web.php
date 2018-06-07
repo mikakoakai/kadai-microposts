676453c429c8a35c99402629ab4d76735bc38cd5
mikako.akai <mikako.akai@rakuten.com>
2018-06-07 05:30:18 +0000

    follow, unfollow

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,18 @@ Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
 });
 
-Route::get('/', 'MicropostsController@index');
+Route::get('/', 'TasksController@index');
 
 // omit
 
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+   Route::group(['prefix' => 'users/{id}'], function () {
+        Route::post('follow', 'UserFollowController@store')->name('user.follow');
+        Route::delete('unfollow', 'UserFollowController@destroy')->name('user.unfollow');
+        Route::get('followings', 'UsersController@followings')->name('users.followings');
+        Route::get('followers', 'UsersController@followers')->name('users.followers');
+    });
+
     Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
